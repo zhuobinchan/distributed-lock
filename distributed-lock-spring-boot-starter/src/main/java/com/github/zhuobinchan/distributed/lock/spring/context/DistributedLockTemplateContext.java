1ec24a4386d31edc16fbe4a806c4bdd9805eda7d
陈焯斌 <chenzhuobin@xinpayroll.com>
2020-12-24 14:06:18 +0800

    update:写法调整

--- distributed-lock-spring-boot-starter/src/main/java/com/github/zhuobinchan/distributed/lock/spring/context/DistributedLockTemplateContext.java
+++ distributed-lock-spring-boot-starter/src/main/java/com/github/zhuobinchan/distributed/lock/spring/context/DistributedLockTemplateContext.java
@@ -4,20 +4,16 @@ import com.github.zhuobinchan.distributed.lock.core.core.DistributedLockTemplate
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 /**
  * @author zhuobin chan on 2020-12-23 18:36
  */
 @Component
 public class DistributedLockTemplateContext {
-    @Autowired
-    private DistributedLockTemplate distributedLockTemplate;
 
     private static DistributedLockTemplate staticTemplate;
 
-    @PostConstruct
-    public void initStaticTemplate() {
+    @Autowired
+    public void initStaticTemplate(DistributedLockTemplate distributedLockTemplate) {
         staticTemplate = distributedLockTemplate;
     }
 
